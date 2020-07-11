d48c018b39f9b05a3fb1839ebb47fb6b741fac7f
manikoli <okoliechigozie.yahoo.com>
2020-07-11 16:22:21 +0000

    updated app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 3000;
 app.get('/',(req,res)=>{
-	res.send('Hello World');
+	res.send('Contact domain administrator on +2348188209365 for sale');
 });
 
 app.listen(port, ()=>console.log(`Server running on port ${port}!`));
