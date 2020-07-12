29fb9fc396a6bda57d812c726c28e0f82d33a075
Okolie <okoliechigozie@yahoo.com>
2020-07-12 19:18:49 +0100

    modified comment in app.js

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
-const express = require('express');
+const express = require("express");
 const port = 3000;
 const app = express();
 
-app.get('/',(req,res)=>{
-	res.send("This domain is not for sale. Call or text +2348188209365 if you have the nerve to change my mind");
-} );
+app.get("/", (req, res) => {
+	res.send(
+		"This domain is not for sale. Call or text +2348188209365 if you have the nerve to change my mind"
+	);
+});
 
-app.listen(port,()=>console.log(`Server running on port ${port}!));
+app.listen(port, () => console.log(`Server running on port ${port}!`));
