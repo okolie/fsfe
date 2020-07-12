dc8e5cc04d92915773c9eb3dc238e792bc871baf
okolie <okoliechigozie.yahoo.com>
2020-07-12 02:38:43 +0000

    modified app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 3000;
 app.get('/',(req,res)=>{
-	res.send('Contact domain administrator on +2348188209365 for sale');
+	res.send('Contact domain administrator on +2348188209365 for more info');
 });
 
 app.listen(port, ()=>console.log(`Server running on port ${port}!`));
